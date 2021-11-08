27b25c95edee9a2e74c0830f6461cc56527c49cf
p-vascool <vasko_pan1@mail.bg>
2021-11-08 19:33:57 +0200

    Finished Catalog.API with Mongo DB, starting Basket.API with Redis

---
+++ AspMicroServices/src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly MongoClient mongoClient;
+        private readonly IMongoDatabase database;
+        public CatalogContext(IConfiguration configuration)
+        {
+            this.mongoClient = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            this.database = this.mongoClient.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = this.database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
