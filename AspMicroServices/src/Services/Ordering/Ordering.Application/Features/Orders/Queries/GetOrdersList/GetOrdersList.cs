bd9f964044d1183476ccabd0c312b920252eeb9e
p-vascool <vasko_pan1@mail.bg>
2021-11-22 22:11:26 +0200

    Added Repository, UpdateCommand, CheckoutCOmmand and EmailFunctionality

---
+++ AspMicroServices/src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersList.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersList : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+        public GetOrdersList(string userName)
+        {
+            this.UserName = userName;
+        }
+    }
+}
---
+++ AspMicroServices/src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersList, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            this._orderRepository = orderRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersList request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUsername(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
